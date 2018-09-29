001f2315cefe514907523bd9a90d9712f9b1ca2c
jeffansay <31795555+jeffansay@users.noreply.github.com>
2018-09-29 22:52:08 +0800

    initial commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://proto:admin1@ds115653.mlab.com:15653/protodev'
+}
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// DB config
+const db = require('./config/keys').mongoURI;
+
+// connect to mongodb
+mongoose.connect(db, { useNewUrlParser: true })
+        .then(() => {
+            console.log('DB connected Success!');
+            
+        }).catch(err => console.log('DB fail!'));
+
+
+app.get('/', (req, res) => res.send('Hellow'));
+
+// Use routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+
+app.listen(port, () => console.log(`Running on port ${port}`));
