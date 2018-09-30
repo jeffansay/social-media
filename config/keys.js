819e8b431fad18f3dad644871bf30deb19e4e685
jeffansay <31795555+jeffansay@users.noreply.github.com>
2018-09-30 08:43:02 +0800

    adding login routes and token

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://proto:admin1@ds115653.mlab.com:15653/protodev'
+    mongoURI: 'mongodb://proto:admin1@ds115653.mlab.com:15653/protodev',
+    secretOrKey: 'secret'
 }
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
@@ -8,6 +9,9 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+// Body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 // DB config
 const db = require('./config/keys').mongoURI;
 
